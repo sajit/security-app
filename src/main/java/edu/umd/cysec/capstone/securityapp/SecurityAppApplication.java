8850b3409d8d48dedb5120dcc1d536368e2848ed
sajit <sajit.kunnumkal@verato.com>
2023-03-04 10:53:37 -0500

    Security

---
+++ src/main/java/edu/umd/cysec/capstone/securityapp/SecurityAppApplication.java
@@ -0,0 +1,13 @@
+package edu.umd.cysec.capstone.securityapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/umd/cysec/capstone/securityapp/SecurityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.umd.cysec.capstone.securityapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
